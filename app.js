6e56feeed3760489fcbea2841973d843f87af5d0
shimp817 <shimp@oregonstate.edu>
2021-03-30 18:16:30 -0400

    attached event listener to startButton to hide overlay

---
+++ app.js
@@ -0,0 +1,30 @@
+const overlay = document.getElementById('overlay');
+const startButton = document.getElementsByClassName('btn__reset')[0];
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const missed = 0;
+const phrasesArray = ['terrace', 'presence', 'ballet', 'custody', 'admire'];
+
+const getRandomPhraseAsArray = arr => {
+  
+}
+
+const addPhraseToDisplay = arr => {
+
+}
+
+const checkLetter = arr => {
+
+}
+
+const checkWin = arr => {
+
+}
+
+startButton.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
+
+qwerty.addEventListener('click', e => {
+
+});
